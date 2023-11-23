be69d4b34bbe8039cca32cd97ab092f732011223
Elder453 <elder.veliz@yale.edu>
2023-11-23 14:46:03 -0500

    retrieve relevant variables from API. TO DO: create T:S ratio variable, clean, and merge data.

--- src/get_data.R
+++ src/get_data.R
@@ -15,16 +15,43 @@ library(tidyverse)
 ################################
 # 1 - Query API endpoints
 ################################
-# Note: fips = 8 corresponds to CT
+# Note: 
+#   - fips = 9 corresponds to CT
+#   - grade_edfacts = 9 corresponds to grades 9-12
+#   - school_level = 3 corresponds to High school
+
+# math midpoint proficiency
 ct_math <- get_education_data(level = "schools", 
                               source = "edfacts",
                               topic = "assessments",
                               filters = list(grade_edfacts = 9,
                                              fips = 9))
+ct_math <- ct_math %>% select(ncessch, year, math_test_pct_prof_midpt)
 
-# directory
 
-# school finance
+# title i eligibility
+ct_title_ <- get_education_data(level = "schools", 
+                                 source = "ccd",
+                                 topic = "directory",
+                                 filters = list(school_level = 3,
+                                                fips = 9))  
+ct_title_i <- ct_title_i %>% select(ncessch, year, title_i_eligible)
+
+# number of FT teachers
+ct_num_teachers <- get_education_data(level = "schools",
+                                       source = "crdc",
+                                       topic = "teachers-staff",
+                                       filters = list(fips = 9))
+ct_num_teachers <- ct_num_teachers %>% select(ncessch, year, teachers_fte_crdc)
+
+# student enrollment
+ct_enrollment <- get_education_data(level = "schools",
+                                    source = "ccd",
+                                    topic = "enrollment",
+                                    filters = list(fips = 9,
+                                                   year = 2008:2021,
+                                                   grade = 9:12))
+ct_enrollment <- ct_enrollment %>% select(ncessch, year, enrollment) %>% filter(enrollment > 10)
 
 ################################
 # 2 - Build dataset
