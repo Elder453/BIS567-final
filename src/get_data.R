391afb020b34681a2576f9d74738c126924a3a0d
Elder453 <elder.veliz@yale.edu>
2023-11-25 12:23:57 -0500

    dataset built TO DO: clean data set (e.g. enrollment, ratio, math proficiency)

--- src/get_data.R
+++ src/get_data.R
@@ -28,9 +28,8 @@ ct_math <- get_education_data(level = "schools",
                                              fips = 9))
 ct_math <- ct_math %>% select(ncessch, year, math_test_pct_prof_midpt)
 
-
 # title i eligibility
-ct_title_ <- get_education_data(level = "schools", 
+ct_title_i <- get_education_data(level = "schools", 
                                  source = "ccd",
                                  topic = "directory",
                                  filters = list(school_level = 3,
@@ -42,7 +41,9 @@ ct_num_teachers <- get_education_data(level = "schools",
                                        source = "crdc",
                                        topic = "teachers-staff",
                                        filters = list(fips = 9))
-ct_num_teachers <- ct_num_teachers %>% select(ncessch, year, teachers_fte_crdc)
+ct_num_teachers <- ct_num_teachers %>% 
+                    select(ncessch, year, teachers_fte_crdc) %>% 
+                    mutate(teachers_fte_crdc = as.integer(teachers_fte_crdc))
 
 # student enrollment
 ct_enrollment <- get_education_data(level = "schools",
@@ -51,8 +52,30 @@ ct_enrollment <- get_education_data(level = "schools",
                                     filters = list(fips = 9,
                                                    year = 2008:2021,
                                                    grade = 9:12))
-ct_enrollment <- ct_enrollment %>% select(ncessch, year, enrollment) %>% filter(enrollment > 10)
+ct_enrollment <- ct_enrollment %>%
+                  group_by(year, ncessch) %>%
+                  summarize(stud_enrollment = sum(enrollment)) %>% 
+                  arrange(ncessch) %>%
+                  ungroup()
 
 ################################
 # 2 - Build dataset
 ################################
+
+ct <- ct_math %>%
+      arrange(ncessch) %>%
+      left_join(ct_title_i, by = c("ncessch", "year")) %>%
+      left_join(ct_num_teachers, by = c("ncessch", "year")) %>%
+      left_join(ct_enrollment, by = c("ncessch", "year")) %>% 
+      filter(!is.na(teachers_fte_crdc), !is.na(stud_enrollment), !is.na(title_i_eligible)) %>%
+      mutate(student_teacher_ratio = if_else(
+                                              !is.na(teachers_fte_crdc) & !is.na(stud_enrollment), # only needed if don't add filter() line
+                                              stud_enrollment / teachers_fte_crdc, 
+                                              NA_real_))
+
+################################
+# 3 - Clean dataset
+################################
+
+hist(subset(ct, stud_enrollment < 100)$stud_enrollment)
+
