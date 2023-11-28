d2e4a4a024cd42efbfc1126ba989c3a400522633
Elder453 <elder.veliz@yale.edu>
2023-11-28 15:44:23 -0500

    added counties, lat, lon.
    logit transformed response
    
    created 2 matrices (indicator for ncessch and indicator for ncessch in county)

--- src/get_data.R
+++ src/get_data.R
@@ -60,6 +60,18 @@ ct_enrollment <- ct_enrollment %>%
                   arrange(ncessch) %>%
                   ungroup()
 
+geo <- get_education_data(level = "schools",
+                           source = "nhgis",
+                           topic = "census-2010",
+                           filters = list(fips = FIPS,
+                                          year = 2011:2017))
+
+geo <- geo %>%
+        group_by(ncessch) %>%
+        slice(1) %>%
+        mutate(county = as.factor(county_code)) %>%
+        select(ncessch, longitude, latitude, county)
+
 ################################
 # 2 - Clean & Build dataset
 ################################
@@ -68,7 +80,9 @@ temp <- ct_math %>%
       arrange(ncessch) %>%
       left_join(ct_title_i, by = c("ncessch", "year")) %>%
       left_join(ct_num_teachers, by = c("ncessch", "year")) %>%
-      left_join(ct_enrollment, by = c("ncessch", "year"))
+      left_join(ct_enrollment, by = c("ncessch", "year")) %>%
+      left_join(geo, by = c("ncessch")) %>%
+      mutate(ncessch = as.factor(ncessch))
 
 # Impute teachers_fte_crdc
 impute_teachers_fte <- function(df, year, school_id) {
@@ -106,7 +120,8 @@ ny <- temp %>%
       mutate(student_teacher_ratio = if_else(
                                               !is.na(teachers_fte_crdc) & !is.na(stud_enrollment), # only needed if don't add filter() line
                                               stud_enrollment / teachers_fte_crdc, 
-                                              NA_real_))
+                                              NA_real_)) %>%
+      arrange(ncessch)
 
 ggplot(ny, aes(x = year, y = math_test_pct_prof_midpt, group = ncessch, color = as.factor(ncessch))) +
   geom_line() +
@@ -117,17 +132,84 @@ ggplot(ny, aes(x = year, y = math_test_pct_prof_midpt, group = ncessch, color = 
   theme_minimal() + 
   theme(legend.position = "none")
 
-# distribution/outlier threshold --> need to log transform response (midpt math %)
-hist(ny$math_test_pct_prof_midpt)
-mean(ny$math_test_pct_prof_midpt) - 3*sd(ny$math_test_pct_prof_midpt)
+# logit transform response 
+ny$logit_math_midpt <- log( (ny$math_test_pct_prof_midpt/100) / (1 - ny$math_test_pct_prof_midpt/100) )
+hist(ny$logit_math_midpt)
 
-# dist/potential outliers (student enrollment)
-hist(ny$stud_enrollment)
-mean(ny$stud_enrollment) + 3*sd(ny$stud_enrollment)
 # 2013: NY state adopts tougher benchmarks for proficiency (adjusting to Common Core) --> 2013 has huge declines in relate to 2012 but recovers 2014
   # indicator variable for 2013 (common core adoption)
 
 # Save data as CSV
-#save(ny, file = "final_data.rdata")
+save(ny, file = "final_data.rdata")
+
+
+
+
+
+# Ensure that 'ncessch' and 'county' are factors
+ny$ncessch <- as.factor(ny$ncessch)
+ny$county <- as.factor(ny$county)
+
+# Get the number of unique 'ncessch' and 'county'
+num_ncessch <- length(unique(ny$ncessch))
+num_county <- length(unique(ny$county))
+
+# Create a matrix of zeros with the correct dimensions
+matrix_ncessch_county <- matrix(0, nrow = num_ncessch, ncol = num_county)
+
+# Name the rows and columns of the matrix
+rownames(matrix_ncessch_county) <- sort(unique(ny$ncessch))
+colnames(matrix_ncessch_county) <- sort(unique(ny$county))
+
+# Populate the matrix
+for (i in 1:nrow(ny)) {
+  ncessch_index <- which(rownames(matrix_ncessch_county) == ny$ncessch[i])
+  county_index <- which(colnames(matrix_ncessch_county) == ny$county[i])
+  matrix_ncessch_county[ncessch_index, county_index] <- 1
+}
+
+# The matrix is now ready
+matrix_ncessch_county
+
+
+
+
+# Get the number of observations and the number of unique 'ncessch'
+num_observations <- nrow(ny)
+num_ncessch <- length(levels(ny$ncessch))
+
+# Create a matrix of zeros with the correct dimensions
+indicator_matrix <- matrix(0, nrow = num_observations, ncol = num_ncessch)
+
+# Name the cols of the matrix after each unique 'ncessch'
+colnames(indicator_matrix) <- levels(ny$ncessch)
+
+# Populate the matrix
+for (i in 1:num_observations) {
+  ncessch_value <- ny$ncessch[i]
+  column_index <- which(colnames(indicator_matrix) == ncessch_value)
+  indicator_matrix[i, column_index] <- 1
+}
+
+indicator_matrix
+
+
+
+
+
+
+
+# ST.CARanova()
+# ranodm effect that is time (county-specific), learns about space and time separately (can turn off interaction term) 
+# replication --> multi-level version: multivariate: k = county, t= time, j = school;
+# MVST.CARar()
+# AR1 at each time point there's specific set of effects (e.g. counties); vectors subjected to auto reg structure; in anova there's one static set of spatial and temporal paramteters. 
+# hard to learn correlation over time; won't be able to learn about rho t (pg 15); year effect
+# ignore time --> dummy for vars
+
+# JAGS, can't do spatial part but can get hierarchical structure
+
+# shape file for counties to use carBayes
+
 
 
